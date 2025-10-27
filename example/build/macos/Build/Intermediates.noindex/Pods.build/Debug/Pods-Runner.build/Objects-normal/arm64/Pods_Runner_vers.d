412b18260c0225ce291db2a6419baa1441780a45
Víctor Manuel Palmero Valdés <palmerodev@gmail.com>
2025-10-27 15:49:00 -0400

    Remove ignored dirs from tracking

--- example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/TuMaletin/Victor/04-Applications/flutter_const/example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Volumes/TuMaletin/Victor/04-Applications/flutter_const/example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
--- example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/Objects-normal/arm64/url_launcher_macos_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/TuMaletin/Victor/04-Applications/flutter_const/example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/module.modulemap \
-  /Volumes/TuMaletin/Victor/04-Applications/flutter_const/example/build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/DerivedSources/url_launcher_macos_vers.c
